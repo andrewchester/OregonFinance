0c4cdcf5cfc9073bc5226ae89c2218bfce8c2156
andrewchester <achester652@gmail.com>
2022-11-07 20:06:57 -0800

    Added time series graph

--- process_xls.py
+++ process_xls.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import sys
 
+pd.options.mode.chained_assignment = None
+
 # require a command line argument
 xlspath = sys.argv[1]
 
@@ -10,6 +12,7 @@ xlspath = sys.argv[1]
 finances = pd.read_excel(xlspath)
 
 # comes from Oregon's published ORESTAR reporting guide
+# 1 indicates income, -1 indicates spending
 transaction_t = {
     'Cash Contribution': 1,
     'In-Kind Contribution': 1,
@@ -38,7 +41,9 @@ funding_percentages = {}
 untracked = {}
 
 # keep track of income and spending over time
-timeline = np.arange(pd.to_datetime(finances['Tran Date'].min()), pd.to_datetime(finances['Tran Date'].max()), dtype="datetime64[D]")
+start_date = pd.to_datetime(finances['Tran Date'].min())
+end_date = pd.to_datetime(finances['Tran Date'].max())
+timeline = np.arange(start_date, end_date, dtype="datetime64[D]")
 income_dt = np.zeros(timeline.size)
 spending_dt = np.zeros(timeline.size)
 
@@ -46,24 +51,26 @@ spending_dt = np.zeros(timeline.size)
 cash_on_hand = 0
 total_funds_received = 0
 
+# iterate over all rows and compute values
 for i, row in finances.iterrows():
     t, n = row['Sub Type'], row['Amount']
     d = pd.to_datetime(row['Tran Date'])
     dateidx = np.where(timeline == d)[0]
 
+    # tracked transaction type
     if t in transaction_t:
         cash_on_hand += transaction_t[t]*n
 
-        if transaction_t[t] == 1:
+        if transaction_t[t] == 1: # income
             total_funds_received += n
             income_dt[dateidx] += n
             if t in funding_composition:
                 funding_composition[t] += n
             else:
                 funding_composition[t] = n
-        else:
+        else: # spending
             spending_dt[dateidx] += n
-    else:
+    else: # untracked type of transaction
         if t in untracked:
             untracked[t] += 1
         else:
@@ -71,3 +78,18 @@ for i, row in finances.iterrows():
     
 for key, value in funding_composition.items():
     funding_percentages[key] = 100*(value / total_funds_received)
+
+scaled_income_dt = income_dt / 1000
+scaled_spending_dt = spending_dt / 1000
+
+# plot income and spending over time
+plt.plot(timeline, scaled_income_dt, label='income')
+plt.plot(timeline, scaled_spending_dt, label='spending', color='red')
+
+plt.title("Income and spending per day")
+plt.xlabel('Date')
+plt.ylabel('Dollars per day (thousands)')
+plt.xticks(rotation=30, ha='right')
+plt.legend()
+
+plt.show()
